f0c77033106fc5f58bb1c7ebc185d890a69737e3
Yassine Khelifi <datashinobi@users.noreply.github.com>
2020-03-24 08:13:24 +0000

    tf config rename

--- mrcnn/distributed_model.py
+++ mrcnn/distributed_model.py
@@ -1846,8 +1846,13 @@ class MaskRCNN():
 
         tf_config = tf.ConfigProto()
         tf_config.gpu_options.allow_growth = True
-        tf_config.gpu_options.visible_device_list = str(hvd.local_rank()) if self.config.GPU_COUNT == 1 else\ 
-                        '%d,%d' % (hvd.local_rank() * self.config.GPU_COUNT, hvd.local_rank() * self.config.GPU_COUNT + 1)
+        device_list =''
+        if self.config.GPU_COUNT == 1:
+            device_list = str(hvd.local_rank())
+        else
+             device_list = '%d,%d' % (hvd.local_rank() * self.config.GPU_COUNT, hvd.local_rank() * self.config.GPU_COUNT + 1)
+             
+        tf_config.gpu_options.visible_device_list = str(hvd.local_rank()) 
         K.set_session(tf.Session(config=tf_config))
 
         self.keras_model = self.build(mode=mode, config=config)
